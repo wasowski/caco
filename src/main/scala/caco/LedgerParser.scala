6a68f3db5814307b0f3796c2f9a494f63647a46b
Andrzej Wąsowski <wasowski@data.pl>
2018-01-07 21:33:32 +0100

    parser accepts a non-trivial file

---
+++ src/main/scala/caco/LedgerParser.scala
@@ -0,0 +1,194 @@
+package caco
+
+import org.parboiled2._
+import shapeless.{::,HNil}
+import caco.model.ledger._
+
+case class LedgerParser (val input: ParserInput, val fname: String) extends Parser  {
+
+  private def loc (cursor: Int) = Location(fname, cursor)
+
+  def main :Rule1[Ledger] = pLedger
+
+  def pLedger = rule { WS ~ zeroOrMore (pLine) ~ EOI ~> { _.toList }  }
+
+  def pLine: Rule1[Line] = rule {
+    pOperation | pInvariant | pAssertion | pUnit | pAccount }
+
+  def pUnit: Rule1[Unit] = rule {
+    push(cursor) ~ UNIT ~ ID ~ (PRECISION ~ INT).? ~ pDescription ~> {
+      (cu: Int, id: Id, pr: Option[Int], de: Description) =>
+        Unit (id, de, loc(cu), pr getOrElse 2) }
+  }
+
+  def pInvariant :Rule1[Invariant] = rule {
+    push(cursor) ~ pDate ~ INVARIANT ~ pExpr ~ pDescription ~>
+      { (cu: Int, da: Date, ex: Expr, de: Description) =>
+        Invariant (ex, da, de, loc(cu)) }
+  }
+
+  def pAssertion :Rule1[Assertion] = rule {
+    push(cursor) ~ pDate ~ pExpr ~ pDescription ~>
+      { (cu: Int, da: Date, ex: Expr, de: Description) =>
+        Assertion (ex, da, de, loc(cu)) }
+  }
+
+  def pAccount: Rule1[Account] = rule { pActiveAccount | pDerivedAccount }
+
+  def pActiveAccount: Rule1[ActiveAccount] = rule {
+    push (cursor) ~ ACCOUNT ~ ID ~ LBRK ~ ID ~ RBRK ~ pDescription ~> {
+      (cu: Int, ac: Id, un: Id, de: Description) =>
+        ActiveAccount (id=ac, unit=un, descr=de, loc=loc(cu)) }
+  }
+
+  def pDerivedAccount: Rule1[DerivedAccount] = rule {
+    push (cursor) ~ ACCOUNT ~ ID ~ EQ ~ pExpr ~ pDescription ~> {
+      (cu: Int, ac: Id, _: BOp, ex: Expr, de: Description) =>
+        DerivedAccount (id=ac, value=ex, descr=de, loc=loc(cu)) }
+  }
+
+  def pOperation = rule { pOperation1 | pOperation2 }
+
+  def pOperation1: Rule1[Operation] = rule {
+    push (cursor) ~ optional(PENDING) ~ pDate ~ pAccounts ~ PLUSEQ ~ pExpr ~ optional (EQMINUS ~ pAccounts) ~
+    pDescription ~>
+    { (cu: Int, pe: Option[Boolean], da: Date, pa: Seq[Id],
+       ex: Expr, ma: Option[Seq[Id]], de: Description) =>
+        Operation(
+          src = pa.toList, tgt = (ma getOrElse Nil).toList,
+          value = ex, tstamp = da, descr = de,
+          loc = loc(cu),
+    pending = !pe.isEmpty) }
+  }
+
+  def pOperation2: Rule1[Operation] = rule {
+    push (cursor) ~ optional(PENDING) ~ pDate ~ pAccounts ~ MINUSEQ ~ pExpr ~ optional (EQPLUS ~ pAccounts) ~
+    pDescription ~>
+    { (cu: Int, pe: Option[Boolean], da: Date, ma: Seq[Id],
+      ex: Expr, opa: Option[Seq[Id]], de: Description) =>
+        Operation(
+          src = (opa getOrElse Nil).toList,
+          tgt = ma.toList,
+          value = ex,
+          tstamp = da,
+          descr = de,
+          loc = loc (cu),
+          pending = !pe.isEmpty) }
+  }
+
+  private def oneDescription :Rule1[String] = rule {
+    PIPE ~ capture(zeroOrMore (noneOf("\n"))) ~ ("\n" ~ WS | EOI) }
+
+  def pDescription :Rule1[Description] = rule {
+    oneDescription.* ~> { (de: Seq[String]) => de.toList }
+  }
+
+  def pAccounts :Rule1[Seq[Id]] = rule { ID.+ separatedBy COMMA }
+
+
+
+  // Expression language
+
+  def pExpr:     Rule1[Expr] = rule { pBoolExpr | pTermExpr }
+
+  def pBoolExpr: Rule1[Expr] = rule { pCompExpr ~ addComp.* }
+  def addComp :Rule[Expr :: HNil, Expr :: HNil] = rule { BOOLOP ~ pCompExpr ~> bexpr _ }
+
+  def pCompExpr: Rule1[Expr] = rule {
+    pTermExpr ~ COMPOP ~ pTermExpr ~> {(l:Expr,o:BOp,r:Expr) => BExpr(l,r,o)}}
+
+  def pTermExpr: Rule1[Expr] = rule { pTerm ~ addTerm.* }
+  def bexpr (l: Expr, o: BOp, r: Expr) :Expr = BExpr (l,r,o)
+  def addTerm :Rule[Expr :: HNil, Expr :: HNil] = rule { TERMOP ~ pTerm ~> bexpr _ }
+
+  // we don't have multiplication but we prime for it in the future
+  def pTerm:   Rule1[Expr]  = rule { pFactor }
+  def pFactor: Rule1[Expr]  = rule { pAmount | pParens | pRef }
+  def pRef:    Rule1[Ref]   = rule { ID ~> { (id:String) => Ref (id) }  }
+  def pParens: Rule1[Expr]  = rule { LPAR ~ pExpr ~ RPAR }
+  def pAmount: Rule1[Const] = rule {
+    FINUM ~> { (nprec: (Long,Int)) => Const(nprec._1,nprec._2) } }
+
+
+
+  // tokens
+
+  private def kwd (st: String): Rule0 = rule {
+    atomic(st) ~ (WS | test(cursorChar == EOI)) }
+  private def kwd1 (st: String): Rule0 = rule {
+    atomic(st) ~ (WS1 | test(cursorChar == EOI)) }
+
+  def WS:  Rule0 = rule { (COMMENT | (anyOf(" \t\n")).+).* }
+  def WS1: Rule0 = rule { (COMMENT | (anyOf(" \t\n")).+).+ }
+
+  def COMMENT: Rule0 = rule { "--" ~ (noneOf("\n")).* ~ ("\n" | test(cursorChar == EOI)) }
+
+  def ID: Rule1[Id] = rule {
+    (capture(
+      (CharPredicate.Alpha | "_") ~
+        zeroOrMore (CharPredicate.AlphaNum | "_" )
+    ) ~ WS) }
+
+  def COMMA = kwd (",")
+  def PIPE  = rule { atomic("|")      }
+  def LPAR  = kwd ("(")
+  def RPAR  = kwd (")")
+  def LBRK  = kwd ("[")
+  def RBRK  = kwd ("]")
+  def MINUS = rule { atomic("-" ~ WS) ~ push (BOp_MINUS) }
+  def PLUS  = rule { atomic("+" ~ WS) ~ push (BOp_PLUS)   }
+
+  def PLUSEQ  = kwd ("+=")
+  def EQPLUS  = kwd ("=+")
+  def MINUSEQ = kwd ("-=")
+  def EQMINUS = kwd ("=-")
+
+  def EQ  = rule { atomic("==" ~ WS) ~ push (BOp_EQ) }
+  def LT  = rule { atomic("<"  ~ WS) ~ push (BOp_LT) }
+  def LTE = rule { atomic("<=" ~ WS) ~ push (BOp_LTE) }
+  def GT  = rule { atomic(">"  ~ WS) ~ push (BOp_GT) }
+  def GTE = rule { atomic(">=" ~ WS) ~ push (BOp_GTE) }
+  def AND = rule { atomic("&&" ~ WS) ~ push (BOp_AND) }
+  def OR = rule { atomic("||" ~ WS) ~ push (BOp_OR) }
+
+  def TERMOP :Rule1[BOp] = rule {  MINUS | PLUS }
+  def COMPOP :Rule1[BOp] = rule { EQ | LT | LTE | GT | GTE }
+  def BOOLOP :Rule1[BOp] = rule { AND | OR }
+
+
+  def pDate  :Rule1[Date] = rule { (pDate8 | pDate6) ~ WS }
+  def pDate8 = rule { push (cursor) ~
+    capture(8 times CharPredicate.Digit) ~> { (c:Int, x:String) => Date (x,loc (c)) } }
+  def pDate6 = rule { push (cursor) ~
+    capture(6 times CharPredicate.Digit) ~> { (c:Int, x:String) => Date ("20"+x,loc (c)) } }
+
+  def INT = rule { INTSTR ~ WS ~> { _.toInt } }
+
+  def FINUM: Rule1[(Long,Int)] = rule {
+    FINUM_PRE ~ optional(FIXEDPNO_SUF) ~ WS ~> {
+    (pre: String, suf: Option[String]) =>
+      val dec = suf getOrElse ""
+      ((pre+dec).toLong, dec.size) }
+  }
+
+  private def INTSTR: Rule1[String] = rule { capture(CharPredicate.Digit.+) }
+
+  private def FINUM_PRE  = rule {
+    INTSTR.+.separatedBy (",") ~> { (cs:Seq[String]) => cs.mkString }
+  }
+
+  private def FIXEDPNO_SUF: Rule1[String] = rule {
+    '.' ~ INTSTR ~> { (s: String) => s.replaceAll("0+$", "") }
+  }
+
+  def UNIT      = kwd1 ("unit")
+  def INVARIANT = kwd1 ("invariant")
+  def PRECISION = kwd1 ("precision")
+  def ACCOUNT   = kwd1 ("account")
+
+  // not sure how I can otherwise make a rule that informed whether it matched
+  // (there should be an easier way thay having the rule to push onto stack,
+  //  but could not figure out)
+  def PENDING   :Rule1[Boolean] = rule { "pending" ~ WS1 ~ push (true) }
+}
+
--- src/test/scala/caco/LedgerParserSpec.scala
+++ src/test/scala/caco/LedgerParserSpec.scala
@@ -2,6 +2,7 @@ package caco
 
 import org.scalatest.{FreeSpec, Matchers,Inside}
 import scala.util.{Success,Failure}
+import reflect.io.Path._
 
 class LedgerParserSpec extends FreeSpec with Matchers with Inside {
 
@@ -9,7 +10,7 @@ class LedgerParserSpec extends FreeSpec with Matchers with Inside {
 
   val fname = "MOCK"
 
-  "token rules" - {
+  "tokens" - {
 
     "pDate" in {
 
@@ -20,7 +21,7 @@ class LedgerParserSpec extends FreeSpec with Matchers with Inside {
       { case Success (Date("20171201",_)) => }
 
       LedgerParser ("171201  ", fname).pDate.run() should matchPattern
-      { case Success (Date("20171201",Some(Location("MOCK",0)))) => }
+      { case Success (Date("20171201",_)) => }
 
       LedgerParser ("171201  ", fname).pDate.run() shouldNot matchPattern
       { case Success (Date("171201",_)) => }
@@ -38,8 +39,8 @@ class LedgerParserSpec extends FreeSpec with Matchers with Inside {
     "amounts in fixedpoint financial notation" in {
 
       def pass (s: String) (n: Long, prec: Precision) =
-        inside(LedgerParser (s, fname).FINUM.run())
-        { case Success ((r1,r2)) =>
+        inside(LedgerParser (s, fname).FINUM.run()) {
+          case Success ((r1,r2)) =>
             r1 should equal (n)
             r2 should equal (prec) }
 
@@ -109,12 +110,12 @@ class LedgerParserSpec extends FreeSpec with Matchers with Inside {
 
       LedgerParser ("20180102 pl += 10000.00 =- ferie",fname).pOperation.run() should matchPattern {
         case Success(Operation(
-            List("pl"), List("ferie"), Const(10000,0), Date("20180102",Some(Location("MOCK",0))), Nil, None, false)) => }
+            List("pl"), List("ferie"), Const(10000,0), Date("20180102",_), Nil, _, false)) => }
 
       LedgerParser ("180103 pl -= 75,011.32 | Transfer to beneficiary",fname).pOperation.run() should matchPattern {
         case Success(Operation(
-            Nil, List("pl"), Const(7501132,2), Date("20180103",Some(Location("MOCK",0))),
-            List(" Transfer to beneficiary"), None, false)) => }
+            Nil, List("pl"), Const(7501132,2), Date("20180103",_),
+            List(" Transfer to beneficiary"), _, false)) => }
 
       val multline = """180102 skat +=    158.35
                        #| description
@@ -123,12 +124,107 @@ class LedgerParserSpec extends FreeSpec with Matchers with Inside {
 
       LedgerParser (multline,fname).pOperation.run() should matchPattern {
         case Success(Operation(
-            List("skat"), Nil, Const(15835,2), Date("20180102",Some(Location("MOCK",0))),
-            List(" description", " line 1", " line 2"), None, false)) => }
+            List("skat"), Nil, Const(15835,2), Date("20180102",_),
+            List(" description", " line 1", " line 2"), _, false)) => }
+
+    }
+
+    "invariants" in {
+
+      val in00 = """171201 invariant D == bike + ferie
+                    #
+                    #-- Initialize the accounts """ stripMargin '#'
+
+      LedgerParser (in00,fname).pInvariant.run() should matchPattern {
+        case Success(Invariant(
+          BExpr(Ref("D"),BExpr(Ref("bike"),Ref("ferie"),BOp_PLUS),BOp_EQ),
+          Date("20171201",_),Nil,_)
+        ) => }
+
+    }
+
+    "assertions" in {
+
+      val as00 = "180106 self_insurance == 4,056.15 | with a description -- and comment"
+      LedgerParser (as00,fname).pAssertion.run() should matchPattern {
+        case Success(Assertion(
+          BExpr(Ref("self_insurance"),
+          Const(405615,2),BOp_EQ),
+          Date("20180106",_),
+          List(" with a description -- and comment"),_)
+        ) => }
+
+    }
+
+    "units" in {
+      val un00 = "unit DKK precision 42 | danske kroner"
+      LedgerParser (un00,fname).pUnit.run() should matchPattern {
+        case Success(Unit("DKK",List(" danske kroner"),_,42)) => }
+    }
+
+    "active accounts" in {
+      val ac00 = "account pt             [DKK] | a buffer awaiting transfer"
+      LedgerParser (ac00, fname).pAccount.run() should matchPattern {
+        case Success(ActiveAccount("pt","DKK",List(" a buffer awaiting transfer"),_)) =>
+      }
+    }
+
+    "derived accounts" in {
+
+      val ac00 = """account D == bike + skat
+                   #| Allocated funds""" stripMargin '#'
+      LedgerParser (ac00, fname).pAccount.run() should matchPattern {
+        case Success(DerivedAccount("D",BExpr(Ref("bike"),Ref("skat"),BOp_PLUS),List(" Allocated funds"),_)) =>
+      }
+    }
+
+  }
+
+    "ledger level AST tests with snippets" - {
+
+      val cases = List(
+        "--",
+        "-- This is a file with random content, exercising most of the grammar",
+        "171201 invariant D == bike + ferie + flights + self_insurance + skat"
+      )
+
+      def test (ca: String, id: Int) {
+        ("case " + id) in {
+          LedgerParser (ca, fname).pLedger.run() should matchPattern {
+            case Success(_) => }
+        }
+      }
+
+      cases.zipWithIndex.foreach { ci => test(ci._1,ci._2) }
 
     }
 
 
+  "file-based tests" - {
+
+    // Directory in which you should put the test files
+    val dir = "test-files/LedgerParser/"
+    // Test files should match this regex (others are ignored)
+    val r = """.*\.caco$""".r
+
+    def test (na: String) = {
+
+      na in {
+        val sr = scala.io.Source.fromFile (na)
+        var pa = LedgerParser (sr.mkString, na)
+        inside(pa.main.run()) {
+          case Success(_) =>
+          case Failure(er: org.parboiled2.ParseError) =>
+              fail (pa formatError er)
+
+          case _ => fail }
+        sr.close
+      }
+    }
+
+    val cases = dir.toDirectory.files.map (dir + _.name).filter {
+      r.findFirstIn(_).isDefined }.toList.sorted
+    cases foreach (test _)
   }
 
 }
